32c6b227091d4945d33bc740b72f336bbb6e1a76
Paul Stakem <paul.stakem@contino.io>
2018-02-07 14:39:26 +0000

    adding 3 tasks to ansible with corresponding tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_vault_group(host):
+    host.group('vault').exists
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_vault_user(host):
+    host.user('vault').exists
+
+
+def test_vault_binary(host):
+    vault_binary = host.file('/usr/local/bin/vault')
+
+    assert vault_binary.exists
